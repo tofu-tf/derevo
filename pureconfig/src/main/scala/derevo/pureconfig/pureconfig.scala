c99db644152b14f0392c48e326556ad0434716ac
Oleg Nizhnik <odomontois@gmail.com>
2020-04-08 15:19:48 +0300

    add magnolia derivation for pureconfig

--- pureconfig/src/main/scala/derevo/pureconfig/pureconfig.scala
+++ pureconfig/src/main/scala/derevo/pureconfig/pureconfig.scala
@@ -12,3 +12,13 @@ object pureconfigReader extends Derivation[ConfigReader] {
 object pureconfigWriter extends Derivation[ConfigWriter] {
   def instance[A]: ConfigWriter[A] = macro Derevo.delegate[ConfigWriter, A]
 }
+
+@delegating("pureconfig.module.magnolia.semiauto.reader.deriveReader")
+object config extends Derivation[ConfigReader] {
+  def instance[A]: ConfigReader[A] = macro Derevo.delegate[ConfigReader, A]
+}
+
+@delegating("pureconfig.module.magnolia.semiauto.writer.deriveWriter")
+object configWriter extends Derivation[ConfigWriter] {
+  def instance[A]: ConfigWriter[A] = macro Derevo.delegate[ConfigWriter, A]
+}
