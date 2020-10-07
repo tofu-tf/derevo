fff28016061403c7ba196a72b8218b14aa7c7a52
A.Voi <voytsishevskiy@tochka.com>
2020-10-07 14:03:33 +0500

    add showNewline;

--- cats/src/main/scala/derevo/cats/show.scala
+++ cats/src/main/scala/derevo/cats/show.scala
@@ -4,21 +4,74 @@ package cats
 import _root_.cats.Show
 import magnolia.{CaseClass, Magnolia, SealedTrait}
 
-object show extends Derivation[Show] {
+/** Generates Show instance for annotated type with field names
+  *
+  * @example
+  * {{{
+  *   import derevo.derive
+  *
+  *   @derive(show)
+  *   case class Foo(bar: Int, baz: String)
+  *
+  *   Show[Foo].show(Foo(3, "abc"))
+  *   //> Foo{bar=3, baz=abc}
+  * }}}
+  */
+object show extends ShowConfigured {
+
+  override def fieldShow(fieldName: String, shownFieldValue: String): String = s"$fieldName=$shownFieldValue"
+
+  override def resultCombine(typeName: String, shownFieldValues: Seq[String]): String = shownFieldValues.mkString(s"$typeName{", ",", "}")
+
+}
+
+/** Generates Show instance for annotated type with newline delimited fields
+  *
+  * Suitable for case classes with many fields  for comortable reading.
+  *
+  * @example
+  * {{{
+  *   import derevo.derive
+  *
+  *   @derive(showNewline)
+  *   case class Foo(bar: Int, baz: String)
+  *
+  *   Show[Foo].show(Foo(3, "abc"))
+  *   //> Foo{
+  *   //    bar = 3,
+  *   //    baz = abc
+  *   //  }
+  * }}}
+  */
+object showNewline extends ShowConfigured {
+
+  override def fieldShow(fieldName: String, shownFieldValue: String): String = s"  $fieldName = $shownFieldValue"
+
+  override def resultCombine(typeName: String, shownFieldValues: Seq[String]): String = shownFieldValues.mkString(s"$typeName{\n", ",\n", "}")
+}
+
+trait ShowConfigured extends Derivation[Show] {
+  def fieldShow(fieldName: String, shownFieldValue: String): String
+
+  def resultCombine(typeName: String, shownFieldValues: Seq[String]): String
+
   type Typeclass[T] = Show[T]
 
-  def combine[T](ctx: CaseClass[Show, T]): Show[T] = new Show[T] {
-    def show(value: T): String = ctx.parameters.map { p =>
-      s"${p.label}=${p.typeclass.show(p.dereference(value))}"
-    }.mkString(s"${ctx.typeName.short}{", ",", "}")
-  }
+  def combine[T](ctx: CaseClass[Show, T]): Show[T] =
+    (value: T) =>
+      resultCombine(ctx.typeName.short, ctx.parameters
+        .map {
+          p =>
+            fieldShow(p.label, s"${p.typeclass.show(p.dereference(value))}")
+        }
+      )
 
   def dispatch[T](ctx: SealedTrait[Show, T]): Show[T] =
-    new Show[T] {
-      def show(value: T): String = ctx.dispatch(value) { sub =>
-        sub.typeclass.show(sub.cast(value))
+    (value: T) =>
+      ctx.dispatch(value) {
+        sub =>
+          sub.typeclass.show(sub.cast(value))
       }
-    }
 
   def instance[T]: Show[T] = macro Magnolia.gen[T]
 }
