28f85b47f53fe1f9e611b03fbd2d1440405c7bc5
Gabriel Volpe <volpegabriel@gmail.com>
2021-04-13 12:25:26 +0200

    Support Circe's KeyDecoder & KeyEncoder

--- circeMagnolia/src/test/scala/derevo/circe/magnolia/CirceDerivationSpec.scala
+++ circeMagnolia/src/test/scala/derevo/circe/magnolia/CirceDerivationSpec.scala
@@ -5,7 +5,7 @@ import io.circe._
 import io.circe.magnolia.configured.Configuration
 import io.circe.syntax._
 import io.circe.parser._
-//import io.circe.derivation.renaming
+import io.estatico.newtype.macros.newtype
 
 import org.scalatest.flatspec.AnyFlatSpec
 
@@ -72,6 +72,58 @@ class CirceDerivationSpec extends AnyFlatSpec {
     assert(decode[SealedTrait](barJson) == Right(bar))
     assert(decode[SealedTrait](bazJson) == Right(baz))
   }
+
+  it should "derive key codecs for Map data-structures" in {
+    import KeyCodecs._
+
+    val kv1     = KVStore(Map(StoreKey(1) -> StoreValue("uno"), StoreKey(2) -> StoreValue("dos")))
+    val kv1Json =
+      """
+       |{
+       |  "1" : "uno",
+       |  "2" : "dos"
+       |}
+       |""".stripMargin.filterNot(_.isWhitespace)
+
+    assert(Encoder[KVStore].apply(kv1).noSpaces == kv1Json)
+    assert(decode[KVStore](kv1Json) == Right(kv1))
+
+    val kv2     = SimpleKV(Map(SimpleKey("jeden") -> SimpleValue(List(1.0)), SimpleKey("dwa") -> SimpleValue(List(2.0))))
+    val kv2Json =
+      """
+       |{
+       |  "kvs" : {
+       |    "jeden" : {
+       |      "value" : [
+       |        1.0
+       |      ]
+       |    },
+       |    "dwa" : {
+       |      "value" : [
+       |        2.0
+       |      ]
+       |    }
+       |  }
+       |}
+       |""".stripMargin.filterNot(_.isWhitespace)
+
+    assert(Encoder[SimpleKV].apply(kv2).noSpaces == kv2Json)
+    assert(decode[SimpleKV](kv2Json) == Right(kv2))
+
+    val kv3     = DeadSimpleKV(Map(0 -> "zero", 1 -> "one"))
+    val kv3Json =
+      """
+       |{
+       |  "kvs" : {
+       |    "0" : "zero",
+       |    "1" : "one"
+       |  }
+       |}
+       |""".stripMargin.filterNot(_.isWhitespace)
+
+    assert(Encoder[DeadSimpleKV].apply(kv3).noSpaces == kv3Json)
+    assert(decode[DeadSimpleKV](kv3Json) == Right(kv3))
+  }
 }
 
 @derive(customizableEncoder, customizableDecoder)
@@ -85,4 +137,31 @@ object SealedTrait {
 
   @derive(encoder, decoder)
   case class Baz(baz: String) extends SealedTrait
+}
+
+object KeyCodecs {
+  @derive(decoder, encoder, keyDecoder, keyEncoder)
+  @newtype
+  case class StoreKey(value: Int)
+
+  @derive(decoder, encoder)
+  @newtype
+  case class StoreValue(value: String)
+
+  @derive(decoder, encoder)
+  @newtype
+  case class KVStore(kvs: Map[StoreKey, StoreValue])
+
+  // no newtypes here
+  @derive(decoder, encoder, keyDecoder, keyEncoder)
+  case class SimpleKey(value: String)
+
+  @derive(decoder, encoder)
+  case class SimpleValue(value: List[Double])
+
+  @derive(decoder, encoder)
+  case class SimpleKV(kvs: Map[SimpleKey, SimpleValue])
+
+  @derive(decoder, encoder)
+  case class DeadSimpleKV(kvs: Map[Int, String])
 }
