3c1e27a3fd5130911e96ec4649069c41be3bf5b8
Grigorii Berezin <grisha.berezin.1998@mail.ru>
2024-04-25 16:59:13 +0300

    feat: add eqv derivation with fields to compare

--- modules/cats/src/main/scala/derevo/cats/eqv.scala
+++ modules/cats/src/main/scala/derevo/cats/eqv.scala
@@ -1,9 +1,10 @@
 package derevo.cats
 
 import cats.Eq
+import derevo.{Derivation, NewTypeDerivation}
 import magnolia.{CaseClass, Magnolia, SealedTrait}
-import derevo.Derivation
-import derevo.NewTypeDerivation
+
+import scala.reflect.macros.blackbox
 
 object eqv extends Derivation[Eq] with NewTypeDerivation[Eq] {
   type Typeclass[T] = Eq[T]
@@ -24,6 +25,31 @@ object eqv extends Derivation[Eq] with NewTypeDerivation[Eq] {
     }
 
   implicit def instance[T]: Eq[T] = macro Magnolia.gen[T]
+
+  def apply[T](eqFields: String*): Eq[T] = macro eqExtendedImpl[T]
+
+  def eqExtendedImpl[T: c.WeakTypeTag](c: blackbox.Context)(eqFields: c.Expr[String]*): c.Tree = {
+    import c.universe._
+
+    val T = weakTypeOf[T]
+    val eqFieldsSet = eqFields.map(_.tree).collect {
+      case Literal(Constant(field: String)) => field
+    }.toSet
+
+    val comparisons = T.decls.collect {
+      case m: MethodSymbol if m.isCaseAccessor && eqFieldsSet.contains(m.name.toString) =>
+        val name = m.name.toTermName
+        q"Eq[${m.typeSignature}].eqv(x.$name, y.$name)"
+    }
+
+    q"""
+        new Eq[$T] {
+          def eqv(x: $T, y: $T): Boolean = {
+            ..$comparisons
+          }
+        }
+      """
+  }
 
   object universal extends Derivation[Eq] {
     implicit def instance[T]: Eq[T] = Eq.fromUniversalEquals[T]
